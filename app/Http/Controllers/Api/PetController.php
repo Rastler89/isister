65fa782699adad5f3eed40055b5a04eaa4fe105c
Daniel Molina (Rastler) <rastler89@gmail.com>
2024-02-12 16:50:29 +0100

    process

--- app/Http/Controllers/Api/PetController.php
+++ app/Http/Controllers/Api/PetController.php
@@ -16,7 +16,20 @@ class PetController extends Controller
 
     //Afegeix una mascota
     public function add(Request $request) {
-        echo $request;
+        $pet = new Pet();
+
+        $pet->user_id = $request->user()->id;
+        $pet->name = $request->get('name');
+        $pet->gender = $request->get('gender');
+        $pet->birth = $request->get('birth');
+        $pet->breed_id = $request->get('breed');
+        $pet->code = $request->get('code');
+        $pet->status = 1;
+
+        $pet->save();
+
+        return response()->json($pet->id);
+
     }
 
     //Contador de mascotes
