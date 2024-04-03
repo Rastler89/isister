0c43419bf68c774e65223b92d77f70d646ad4437
Daniel Molina (Rastler) <rastler89@gmail.com>
2024-04-03 18:35:07 +0200

    species

--- app/Http/Controllers/Api/SpecieController.php
+++ app/Http/Controllers/Api/SpecieController.php
@@ -5,10 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Specie;
+use App\Models\Breed;
 
 class SpecieController extends Controller {
 
     public function getAll() {
-        return response()->json(Specie::all());
+        $species = Specie::all();
+        foreach($species as $specie) {
+            $specie['name'] = json_decode($specie['name']);
+            $breeds = Breed::where('specie_id','=',$specie['id'])->get();
+            foreach($breeds as $breed) {
+                $breed['name'] = json_decode($breed['name']);
+            }
+            $specie['breeds'] = $breeds;
+        }
+
+
+        return response()->json($species);
     }
 }
